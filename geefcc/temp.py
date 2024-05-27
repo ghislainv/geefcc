6830818c4441ff3982206198852da1f39f6f118b
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2024-05-27 11:12:50 +1100

    Comment temp.py

--- geefcc/temp.py
+++ geefcc/temp.py
@@ -1,14 +1,17 @@
-# Adjusted extent
-xmin = aoi[0]
-xmax = aoi[2]
-xmax_tap = xmin + math.ceil((xmax - xmin) / scale) * scale
-ymin = aoi[1]
-ymax = aoi[3]
-ymax_tap = ymin + math.ceil((ymax - ymin) / scale) * scale
+# import math
 
-# projWin = [ulx, uly, lrx, lry]
-gdal.Translate(output_file, vrt_file,
-               maskBand=None,
-               projWin=[xmin, ymax_tap, xmax_tap, ymin],
-               creationOptions=copts,
-               callback=cback)
+
+# # Adjusted extent
+# xmin = aoi[0]
+# xmax = aoi[2]
+# xmax_tap = xmin + math.ceil((xmax - xmin) / scale) * scale
+# ymin = aoi[1]
+# ymax = aoi[3]
+# ymax_tap = ymin + math.ceil((ymax - ymin) / scale) * scale
+
+# # projWin = [ulx, uly, lrx, lry]
+# gdal.Translate(output_file, vrt_file,
+#                maskBand=None,
+#                projWin=[xmin, ymax_tap, xmax_tap, ymin],
+#                creationOptions=copts,
+#                callback=cback)
