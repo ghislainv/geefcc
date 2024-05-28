f7e3a61276ceda22ad6dd316e994251434bcbe47
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2024-05-28 18:54:16 +1100

    Change imports

--- geefcc/__init__.py
+++ geefcc/__init__.py
@@ -15,7 +15,7 @@ from osgeo import gdal
 from .ee_initialize import ee_initialize
 from .get_fcc import get_fcc
 from .sum_raster_bands import sum_raster_bands
-from .misc import make_dir
+from . import misc
 
 # GDAL exceptions
 gdal.UseExceptions()
